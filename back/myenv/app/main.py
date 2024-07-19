d91c24984c0688a647f4f4ea5c7ac88a030482de
lookinmin <ancx1234@naver.com>
2024-07-19 20:06:20 +0900

    dev_setting
    - react ts pwa
    - fastapi sqlalchemy

---
+++ back/myenv/app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app import api
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = {"*"},
+    allow_credentials=True,
+    allow_methods={"OPTIONS", "GET", "POST"},
+    allow_headers={"*"},
+)
+
+@app.on_event("startup")
+def on_startup():
+    from app import models
+    from app.database import engine
+    
+    models.Base.metadata.create_all(bind=engine)
+
+app.include_router(api.router)
