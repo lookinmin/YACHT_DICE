67742feda22a51009c8547654b86bac354839c4f
lookinmin <ancx1234@naver.com>
2024-07-24 21:11:08 +0900

    startPage formatting

--- front/src/pages/StartPage.tsx
+++ front/src/pages/StartPage.tsx
@@ -1,5 +1,8 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import TextField from '@mui/material/TextField';
+import { tfTheme } from '../styles/theme';
+import { ThemeProvider } from '@mui/material/styles';
 
 const StPg = styled.div`
   display: flex;
@@ -8,7 +11,7 @@ const StPg = styled.div`
   width: 100%;
   justify-content: center;
   align-items: center;
-`
+`;
 
 const ChdDiv = styled.div`
   display: grid;
@@ -16,7 +19,7 @@ const ChdDiv = styled.div`
   grid-template-columns: 4fr 6fr;
   column-gap: 1vw;
   place-items: center;
-`
+`;
 
 const ImgDiv = styled.div`
   display: flex;
@@ -25,24 +28,61 @@ const ImgDiv = styled.div`
   justify-content: center;
   padding: 5vh 0;
   gap: 3vh;
-`
+`;
 
 const FormDiv = styled.div`
-`
+  display: flex;
+  flex-flow: column nowrap;
+  gap: 20px;
+  width: 80%;
+  align-items: center;
+
+  & > #txtBtn {
+    width: 98%;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+    & > p {
+      margin: 0 !important;
+      font-weight: 600;
+      cursor: pointer;
+
+      &:hover {
+        color: #90c9ff;
+      }
+    }
+  }
+`;
 
 export const StartPage: React.FC = () => {
   return (
-    <StPg>
-      <ChdDiv>
-      <ImgDiv>
-      <h1>YACHT DICE</h1>
-      </ImgDiv>
-      <FormDiv>
+    <ThemeProvider theme={tfTheme}>
+      <StPg>
+        <ChdDiv>
+          <ImgDiv>
+            <h1>YACHT DICE</h1>
+          </ImgDiv>
+          <FormDiv>
+            <TextField
+              id="outlined-basic"
+              label="ID"
+              variant="outlined"
+              fullWidth
+            />
+            <TextField
+              id="outlined-basic"
+              label="PASSWORD"
+              variant="outlined"
+              fullWidth
+            />
 
-      </FormDiv>
-      </ChdDiv>
-    </StPg>
-  )
-}
-
-
+            <div id="txtBtn">
+              <p>SIGN UP</p>
+              <p>LOGIN</p>
+            </div>
+          </FormDiv>
+        </ChdDiv>
+      </StPg>
+    </ThemeProvider>
+  );
+};
--- front/src/styles/theme.ts
+++ front/src/styles/theme.ts
@@ -1,11 +1,47 @@
-import { DefaultTheme } from "styled-components";
+import { DefaultTheme } from 'styled-components';
+import { createTheme } from '@mui/material';
 
 export const lightTheme: DefaultTheme = {
-    background: '#ffffff',
-    color: '#000000',
-  };
-  
-  export const darkTheme: DefaultTheme = {
-    background: '#262626',
-    color: '#f7f7f7',
-  };
+  background: '#ffffff',
+  color: '#000000',
+};
+
+export const darkTheme: DefaultTheme = {
+  background: '#262626',
+  color: '#f7f7f7',
+};
+
+export const tfTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#87CEEB', // 기본 파란색
+    },
+    secondary: {
+      main: '#00ff00', // 연두색
+    },
+  },
+  components: {
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          '& .MuiOutlinedInput-root': {
+            borderRadius: '10px', // 테두리 반경 설정
+            backgroundColor: '#87CEEB', // 기본 파란색 배경
+            '& fieldset': {
+              borderColor: '#87CEEB', // 기본 파란색 테두리
+            },
+            '&:hover fieldset': {
+              borderColor: '#87CEEB', // 기본 파란색 테두리
+            },
+            '&.Mui-focused': {
+              backgroundColor: '#00ff00', // 포커스 시 연두색 배경
+              '& fieldset': {
+                borderColor: '#00ff00', // 포커스 시 연두색 테두리
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+});
