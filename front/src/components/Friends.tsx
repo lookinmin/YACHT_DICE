3117275eebcf7fb0306db18e04f92a6d9c0df24e
lookinmin <ancx1234@naver.com>
2024-08-05 00:59:29 +0900

    friend list UI

--- front/src/components/Friends.tsx
+++ front/src/components/Friends.tsx
@@ -6,6 +6,11 @@ import { InputAdornment } from '@mui/material';
 import { FaRegCircleUser } from 'react-icons/fa6';
 import { BsSearchHeart } from 'react-icons/bs';
 import IconButton from '@mui/material/IconButton';
+import { RiUserSearchLine, RiUserHeartLine } from 'react-icons/ri';
+import { GoPlusCircle, GoXCircle } from 'react-icons/go';
+import { GiRollingDices } from 'react-icons/gi';
+import { ThemeProvider } from '@mui/material/styles';
+import { tfTheme2 } from '../styles/theme';
 
 const StyledDiv = styled.div`
   display: flex;
@@ -49,35 +54,90 @@ const StyledDiv = styled.div`
   }
 `;
 
+interface FriendProps {
+  isFriend: boolean;
+}
+
+const Friend = styled.div<FriendProps>`
+  display: flex;
+  flex-flow: row nowrap;
+  width: 80%;
+  box-sizing: border-box;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 10px;
+  background-color: ${(props) => (props.isFriend ? '#8ead90' : '#83a9bb')};
+  padding: 0.5vh 1vw;
+  height: 57px;
+  color: ${(props) => (props.isFriend ? '#ffffff' : '#1a64d3')};
+  //color : #86aa9c
+  & > .front {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 20px;
+
+    & > p {
+      font-size: 1.1em;
+      margin: 0 !important;
+    }
+  }
+`;
+
 export const Friends: React.FC = () => {
   return (
-    <StyledDiv>
-      <div className="top">
-        <div className="mark">
-          <PiUsersThreeDuotone size={33} color="skyblue" />
-          <p>Friends List</p>
+    <ThemeProvider theme={tfTheme2}>
+      <StyledDiv>
+        <div className="top">
+          <div className="mark">
+            <PiUsersThreeDuotone size={33} color="skyblue" />
+            <p>Friends List</p>
+          </div>
         </div>
-      </div>
 
-      <form id="search">
-        <TextField
-          id="outlined-basic"
-          label="User ID"
-          variant="outlined"
-          fullWidth
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <FaRegCircleUser size={25} color="#8ccfcf" />
-              </InputAdornment>
-            ),
-            placeholder: 'ENTER ID',
-          }}
-        />
-        <IconButton color="primary" type="submit">
-          <BsSearchHeart size={28} />
-        </IconButton>
-      </form>
-    </StyledDiv>
+        <form id="search">
+          <TextField
+            id="outlined-basic"
+            label="User ID"
+            variant="outlined"
+            fullWidth
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <FaRegCircleUser size={25} color="#8ccfcf" />
+                </InputAdornment>
+              ),
+              placeholder: 'ENTER ID',
+            }}
+          />
+          <IconButton color="primary" type="submit">
+            <BsSearchHeart size={35} />
+          </IconButton>
+        </form>
+
+        <Friend isFriend={false}>
+          <div className="front">
+            <RiUserSearchLine size={30} />
+            <p>ID</p>
+          </div>
+
+          <IconButton color="primary" type="submit" title="ADD Friend">
+            <GoPlusCircle size={36} />
+          </IconButton>
+        </Friend>
+
+        <Friend isFriend={true}>
+          <div className="front">
+            <RiUserHeartLine size={30} />
+            <p>ID</p>
+          </div>
+
+          <IconButton color="inherit" type="submit" title="PLAY!">
+            <GiRollingDices size={44} />
+          </IconButton>
+        </Friend>
+      </StyledDiv>
+    </ThemeProvider>
   );
 };
--- front/src/styles/theme.ts
+++ front/src/styles/theme.ts
@@ -12,6 +12,9 @@ export const darkTheme: DefaultTheme = {
 };
 
 export const tfTheme = createTheme({
+  typography: {
+    fontFamily: 'HancomSans-Light, Arial', // 폴백으로 Arial 지정
+  },
   palette: {
     primary: {
       main: '#ADD8E6 ', // 기본 파란색
@@ -40,6 +43,48 @@ export const tfTheme = createTheme({
               },
             },
           },
+        },
+      },
+    },
+  },
+});
+
+export const tfTheme2 = createTheme({
+  typography: {
+    fontFamily: 'HancomSans-Light, Arial', // 폴백으로 Arial 지정
+  },
+
+  components: {
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          '& .MuiOutlinedInput-notchedOutline': {
+            borderColor: '#86aa9c', // 어두운 녹색 테두리 색상
+            borderWidth: '2px',
+            borderRadius: '10px',
+          },
+          '&:hover .MuiOutlinedInput-notchedOutline': {
+            borderColor: '#86aa9c', // 호버 시에도 같은 테두리 색상 유지
+            borderWidth: '2px',
+            borderRadius: '10px',
+          },
+          '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+            borderColor: '#538ee7', // 포커스 시의 테두리 색상
+            borderWidth: '2px',
+            borderRadius: '10px',
+          },
+        },
+        input: {
+          color: '#ffffff', // 흰색 글씨
+          fontFamily: 'HancomSans-Light, Arial', // 폰트 적용
+        },
+      },
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: {
+          color: '#ffffff', // 흰색 레이블
+          fontFamily: 'HancomSans-Light, Arial', // 폰트 적용
         },
       },
     },
